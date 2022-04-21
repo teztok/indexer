fb5a6b3475cb668216096f7884d38a172e8e4b9b
Simon Kusterer <simon@sope.io>
2022-04-21 18:03:36 +0200

    handle case where the OBJKT_FULFILL_OFFER event refers to a collection offer

--- src/tasks/event-producer/handlers/objkt_fulfill_offer.test.ts
+++ src/tasks/event-producer/handlers/objkt_fulfill_offer.test.ts
@@ -72,6 +72,75 @@ test('creates OBJKT_FULFILL_OFFER events', async () => {
         },
       ],
     },
+    {
+      id: 212174403,
+      level: 2300110,
+      timestamp: '2022-04-21T15:10:59Z',
+      block: 'BLiMKSBbstJynTFMCqP7zAtZpvA4xcLt4zR31BX5L99UtXAyRQ3',
+      hash: 'oobEcddAjV1UWi7EjEgpWT6ZrDLDrGGCRrqxqBdZ7u17PB6WNvT',
+      counter: 41306227,
+      sender: {
+        alias: 'pseudosufism',
+        address: 'tz2WCHSSnwGmXt6CiHUXhLwgZvuEji3sc8Gv',
+      },
+      target: {
+        alias: 'objkt.com Marketplace v2',
+        address: 'KT1WvzYHCNBvDSdwafTHv7nJ1dWmZ8GCYuuC',
+      },
+      amount: 0,
+      parameter: {
+        entrypoint: 'fulfill_offer',
+        value: {
+          offer_id: '1108443',
+          token_id: '2189',
+        },
+      },
+      status: 'applied',
+      hasInternals: true,
+      initiator: null,
+      storage: {
+        asks: 103258,
+        offers: 103260,
+        paused: false,
+        big_map: 103261,
+        metadata: 103259,
+        upgradable: false,
+        next_ask_id: '1347918',
+        next_offer_id: '1114785',
+        token_registry: 'KT1Pbj1uKjxZJLHyoQ2YUs4kukjvLJdheyfU',
+        permission_module: 'KT1DF2c7VUED7W3h5BnqUKdB87Ke7XdCjQiY',
+      },
+      diffs: [
+        {
+          bigmap: 103260,
+          path: 'offers',
+          action: 'remove_key',
+          content: {
+            hash: 'exprthr1quxEvvCrxt7CcXRxEQnSwo664CyHpMiinoWhwFfkScBDPd',
+            key: '1108443',
+            value: {
+              token: {
+                address: 'KT1BqfEQFrfx3h2wWQo7gTM1SE6FpH1Y5pqK',
+                token_id: null,
+              },
+              amount: '8450000',
+              shares: [
+                {
+                  amount: '600',
+                  recipient: 'tz1bKM4FRgAsGdDWzXs4o5HZdjBbLMbPBAA1',
+                },
+              ],
+              target: null,
+              creator: 'tz1Ziaxoio55wxE9UWpXnaMgRtSN3VBhgGTX',
+              currency: {
+                tez: {},
+              },
+              expiry_time: null,
+            },
+          },
+        },
+      ],
+    },
   ];
 
   const events = transactionsToEvents(transactions, [ObjktFulfillOfferHandler]);
@@ -91,7 +160,21 @@ test('creates OBJKT_FULFILL_OFFER events', async () => {
       offer_id: '1000012',
       seller_address: 'tz2PSYEYJff71Vi2qnUd5kUu7efMRzaCEnK2',
       buyer_address: 'tz1aLySy25NTVivCzPhCfHsVwZ38ZT2u7tAi',
-      // artist_address: 'tz1iZ2TPEShFC8TqHsXLA9RXdV7tSv8E3aLe', TODO: add
+    },
+    {
+      id: 'dedc2158555b15d20945bcc271c7e897',
+      type: 'OBJKT_FULFILL_OFFER',
+      implements: 'SALE',
+      opid: 212174403,
+      ophash: 'oobEcddAjV1UWi7EjEgpWT6ZrDLDrGGCRrqxqBdZ7u17PB6WNvT',
+      timestamp: '2022-04-21T15:10:59Z',
+      level: 2300110,
+      price: '8450000',
+      fa2_address: 'KT1BqfEQFrfx3h2wWQo7gTM1SE6FpH1Y5pqK',
+      token_id: '2189',
+      offer_id: '1108443',
+      seller_address: 'tz2WCHSSnwGmXt6CiHUXhLwgZvuEji3sc8Gv',
+      buyer_address: 'tz1Ziaxoio55wxE9UWpXnaMgRtSN3VBhgGTX',
     },
   ]);
 });
