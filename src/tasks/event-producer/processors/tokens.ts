389825a13ef051200c0fe2743f197cfe1ec27071
Simon Kusterer <simon@sope.io>
2022-11-30 16:40:33 +0100

    stricter check regarding which events should be processed

--- src/tasks/event-producer/processors/tokens.ts
+++ src/tasks/event-producer/processors/tokens.ts
@@ -1,9 +1,11 @@
 import uniqBy from 'lodash/uniqBy';
+import get from 'lodash/get';
+import isString from 'lodash/isString';
 import { Processor, TokenEvent } from '../../../types';
 import { getWorkerUtils, getTaskName } from '../../../lib/utils';
 
 const TokenProcessor: Processor = {
-  accept: (event) => 'fa2_address' in event && 'token_id' in event,
+  accept: (event) => isString(get(event, 'fa2_address')) && isString(get(event, 'token_id')),
 
   exec: async (events) => {
     const uniqEvents: Array<TokenEvent> = uniqBy<TokenEvent>(
