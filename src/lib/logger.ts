0445c4b24a2ac61cfffd09e75123ff488e400493
Simon Kusterer <simon@sope.io>
2022-12-23 08:54:59 +0100

    added option to disable file-based logging

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -3,21 +3,17 @@ import path from 'path';
 
 const LOGS_PATH = path.join(__dirname, '../../data/logs');
 
+require('dotenv').config();
+
 const logger = winston.createLogger({
   level: 'info',
   format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
-  transports: [
-    new winston.transports.File({ filename: path.join(LOGS_PATH, 'error.log'), level: 'error' }),
-    new winston.transports.File({ filename: path.join(LOGS_PATH, 'combined.log') }),
-  ],
+  transports: [new winston.transports.Console({ format: winston.format.simple() })],
 });
 
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(
-    new winston.transports.Console({
-      format: winston.format.simple(),
-    })
-  );
+if (!process.env.DISABLE_FILE_LOGGING) {
+  logger.add(new winston.transports.File({ filename: path.join(LOGS_PATH, 'error.log'), level: 'error' }));
+  logger.add(new winston.transports.File({ filename: path.join(LOGS_PATH, 'combined.log') }));
 }
 
 export default logger;
