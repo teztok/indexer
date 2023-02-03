a979b8c697f55e04c4da5c1a3bc128e879866c00
Simon Kusterer <simon@sope.io>
2023-02-03 09:05:08 +0100

    dsiabled quotes-feature (needs further testing)

---
+++ src/scripts/produce-quote-events.ts
@@ -0,0 +1,26 @@
+import '../bootstrap';
+import indexer from '../indexer';
+import { getTaskName } from '../lib/utils';
+import db from '../lib/db';
+
+async function run() {
+
+  const results = await db
+    .select('level')
+    .from('events')
+    .whereNull('quotes')
+    .orderBy('level')
+    .distinctOn('level');
+
+  for (let i = 0; i < results.length; i++) {
+    const result = results[i];
+    console.log(`added level ${result.level} to queue`);
+    await indexer.addJob(getTaskName('event-producer'), {
+      filters: { level: result.level },
+      overwriteEvents: true,
+      overwriteLevel: true,
+    });
+  }
+}
+
+run();
