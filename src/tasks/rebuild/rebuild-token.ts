c2a1048291a960ac7682cf5439ce1c15d6670afb
Simon Kusterer <simon@sope.io>
2023-08-03 11:40:32 +0200

    added platform 'teiacafe' + small refactorings

--- src/tasks/rebuild/rebuild-token.ts
+++ src/tasks/rebuild/rebuild-token.ts
@@ -40,7 +40,7 @@ import {
   TYPED_CONTRACT_MARKETPLACE,
   EIGHTSCRIBO_CONTRACT_MARKETPLACE,
   KALAMINT_CONTRACT_FA2,
-  TEIA_CAFE_FA2,
+  TEIACAFE_FA2,
 } from '../../consts';
 
 export interface RebuildTokenTaskPayload {
@@ -178,25 +178,29 @@ export function compileToken(
 
   let royaltyReceivers = null;
 
-  let teiacafePlaylist = null;
-  let teiacafePlaylistDescription = null;
-  let teiacafePlaylistCover = null;
+  let TeiacafePlaylist = null;
+  let TeiacafePlaylistDescription = null;
+  let TeiacafePlaylistCover = null;
 
   if (metadata && metadata.royalties && is(metadata.royalties, RoyaltySharesSchema)) {
     royaltyReceivers = royaltySharesToRoyaltyReceivers(metadata.royalties);
   }
 
-  if (fa2Address === TEIA_CAFE_FA2 && metadata && metadata.teiacafe_playlist) {
-    if (Array.isArray(get(metadata, 'teiacafe_playlist.playlist'))) {
-      teiacafePlaylist = get(metadata, 'teiacafe_playlist.playlist');
-    }
+  if (fa2Address === TEIACAFE_FA2) {
+    platform = 'TEIACAFE';
 
-    if (isString(get(metadata, 'teiacafe_playlist.playlist_description'))) {
-      teiacafePlaylistDescription = get(metadata, 'teiacafe_playlist.playlist_description');
-    }
+    if (metadata && metadata.teiacafe_playlist) {
+      if (Array.isArray(get(metadata, 'teiacafe_playlist.playlist'))) {
+        TeiacafePlaylist = get(metadata, 'teiacafe_playlist.playlist');
+      }
 
-    if (isString(get(metadata, 'teiacafe_playlist.playlist_cover'))) {
-      teiacafePlaylistCover = get(metadata, 'teiacafe_playlist.playlist_cover');
+      if (isString(get(metadata, 'teiacafe_playlist.playlist_description'))) {
+        TeiacafePlaylistDescription = get(metadata, 'teiacafe_playlist.playlist_description');
+      }
+
+      if (isString(get(metadata, 'teiacafe_playlist.playlist_cover'))) {
+        TeiacafePlaylistCover = get(metadata, 'teiacafe_playlist.playlist_cover');
+      }
     }
   }
 
@@ -1359,9 +1363,9 @@ export function compileToken(
     eightscribo_rowtwo: eightscriboRowtwo,
     eightscribo_rowthree: eightscriboRowthree,
 
-    teiacafe_playlist: teiacafePlaylist,
-    teiacafe_playlist_description: teiacafePlaylistDescription,
-    teiacafe_playlist_cover: teiacafePlaylistCover,
+    teiacafe_playlist: TeiacafePlaylist,
+    teiacafe_playlist_description: TeiacafePlaylistDescription,
+    teiacafe_playlist_cover: TeiacafePlaylistCover,
   };
 
   return {
