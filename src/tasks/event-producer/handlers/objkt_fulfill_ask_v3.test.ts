86f8481164249c4e2c407e3e1bcbcfa94368617a
Simon Kusterer <simon@sope.io>
2024-01-06 08:58:21 +0100

    ignore fulfill asks where amount > 1 for now

--- src/tasks/event-producer/handlers/objkt_fulfill_ask_v3.test.ts
+++ src/tasks/event-producer/handlers/objkt_fulfill_ask_v3.test.ts
@@ -72,6 +72,74 @@ test('creates OBJKT_FULFILL_ASK_V3 events', async () => {
         },
       ],
     },
+    {
+      id: 911995125628929,
+      level: 4870771,
+      timestamp: '2024-01-03T07:59:00Z',
+      block: 'BMAzb7p1ttRVArJvNNoLHHiMqnURRgk6w8HpFT6GMKMpfnPMLKJ',
+      hash: 'ooVWjKFtUnXe77HcvA3N2ZTjaGEse47Pn9zJCSoGJArah879r7W',
+      counter: 80788738,
+      nonce: null,
+      sender: {
+        address: 'tz2MdcJs5yGqGN8xnUhJ6X3Xgzf29cWbLDmZ',
+      },
+      target: {
+        address: 'KT1CePTyk6fk4cFr6fasY5YXPGks6ttjSLp4',
+      },
+      amount: 4000000,
+      parameter: {
+        entrypoint: 'fulfill_ask',
+        value: {
+          amount: '2',
+          ask_id: '10002458',
+          proxy_for: null,
+          referrers: {},
+          condition_extra: null,
+        },
+      },
+      status: 'applied',
+      hasInternals: true,
+      initiator: null,
+      storage: {
+        asks: 574013,
+        offers: 574015,
+        metadata: 574014,
+        next_ask_id: '10002514',
+        next_offer_id: '10000181',
+        permission_module: 'KT1FzwANzQq9RdSXyF7bnC6gFeo5bDX2HP9v',
+        fee_sharing_registry: 'KT1K4xtqQwEp7miTMGq1YXfdcRdjJVyZ7QYK',
+      },
+      diffs: [
+        {
+          bigmap: 574013,
+          path: 'asks',
+          action: 'remove_key',
+          content: {
+            hash: 'expru5eZjSw5pwkuRaoBZTxc7ULgsgEtK2VNcjkFVh7S2DtdmXsUne',
+            key: '10002458',
+            value: {
+              token: {
+                address: 'KT1V6tmo3J2fUr5cnh7K9Q7eaDq3e8i9ucwy',
+                token_id: '133',
+              },
+              amount: '4000000',
+              shares: {
+                tz1Qtpj9fLrfunNwiE4pRjV3mxiR7uJN2Yid: '1000',
+              },
+              creator: 'tz1Qtpj9fLrfunNwiE4pRjV3mxiR7uJN2Yid',
+              currency: {
+                tez: {},
+              },
+              editions: '1',
+              condition: null,
+              expiry_time: null,
+              platform_fee: '500',
+              referral_bonus: '500',
+            },
+          },
+        },
+      ],
+    },
   ];
 
   const events = transactionsToEvents(transactions, [ObjktFulfillAskV3Handler]);
